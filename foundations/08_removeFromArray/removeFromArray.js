624f4fad12090527b940d25e4f932f38981ca7f9
Mihai Sprincenatu <250979906+mihaisprincenatu@users.noreply.github.com>
2026-02-18 13:45:40 +0200

    Complete 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+  for (const num of nums) {
+    while(arr.indexOf(num) !== -1) {
+      arr.splice(arr.indexOf(num), 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
