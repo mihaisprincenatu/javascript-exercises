b2e0c07508fd74dbc179abeb42bb4225509c469e
Mihai Sprincenatu <250979906+mihaisprincenatu@users.noreply.github.com>
2026-02-18 14:16:28 +0200

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (
+    typeof a !== "number" ||
+    typeof b !== "number" ||
+    a < 0 ||
+    b < 0 ||
+    a % 1 !== 0 ||
+    b % 1 !== 0
+  ) {
+    return "ERROR";
+  }
+  if (a > b) {
+    [a, b] = [b, a];
+  }
+  // return b*(b+1)/2 - (a-1)*a/2;
+  arrayFromAToB = [];
+  for (let i=a; i<=b; i++) {
+    arrayFromAToB.push(i);
+  }
+  return arrayFromAToB.reduce((sum, num) => sum + num, 0);
 };
 
 // Do not edit below this line
