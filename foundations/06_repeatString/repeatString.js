98f5c4654a9bda3351e6355953857ca46e5fa1a9
Mihai Sprincenatu <250979906+mihaisprincenatu@users.noreply.github.com>
2026-02-18 13:20:09 +0200

    Complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  else {
+    let repeatStr = "";
+    while (num > 0) {
+      repeatStr = repeatStr.concat(str);
+      num--;
+    }
+    return repeatStr;
+  }
 };
 
 // Do not edit below this line
