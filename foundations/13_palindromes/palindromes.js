1f9da0fcdfcf92bfc4978b29fcd602d30f2877bb
Mihai Sprincenatu <250979906+mihaisprincenatu@users.noreply.github.com>
2026-02-25 14:13:55 +0200

    Complete 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  arr = str
+    .toLowerCase()
+    .split("")
+    .filter((character) => (
+      character >= "a" &&
+      character <= "z" ||
+      character >= "0" &&
+      character <= "9"
+    ))
+  reversedArr = [...arr];
+  reversedArr.reverse();
+  str = arr.join("");
+  reversedStr = reversedArr.join("");
+  return str === reversedStr;  
 };
 
 // Do not edit below this line
