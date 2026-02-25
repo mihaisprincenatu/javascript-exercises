a9018e06acd5d65dd16f933e1b72f2d58b2a50ca
Mihai Sprincenatu <250979906+mihaisprincenatu@users.noreply.github.com>
2026-02-25 14:43:44 +0200

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth_member) {
+  if (+nth_member < 0) {
+    return "OOPS";
+  }
+  if (+nth_member === 0) {
+    return 0;
+  }
+  if (+nth_member === 1) {
+    return 1;
+  }
+  //return (fibonacci(+nth_member - 1) + fibonacci(+nth_member - 2));
+  let prevPrevValue = 0;
+  let prevValue = 1;
+  let value
+  for (let member = 2; member <= +nth_member; member++) {
+    value = prevValue + prevPrevValue;
+    prevPrevValue = prevValue;
+    prevValue = value;
+  }
+  return value;
 };
 
 // Do not edit below this line
