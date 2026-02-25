8cd1f8e0b18ad69e5d0e7856d21f287200370c1f
Mihai Sprincenatu <250979906+mihaisprincenatu@users.noreply.github.com>
2026-02-25 13:23:26 +0200

    Complete 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => sum + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod, num) => prod * num, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1){
+    return 1;
+  }
+  let factorial = 1;
+  while (num > 1){
+    factorial = factorial * num--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
