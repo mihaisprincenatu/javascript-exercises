841f4a3060a881fa01ff2a556b55e39f40489154
Mihai Sprincenatu <250979906+mihaisprincenatu@users.noreply.github.com>
2026-02-25 17:10:09 +0200

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  console.log("start");
+  return people.reduce((oldest, person) => {
+    oldestPersonAge = (("yearOfDeath" in oldest) ? oldest.yearOfDeath : (new Date()).getFullYear()) - oldest.yearOfBirth;
+    newPersonAge = (("yearOfDeath" in person) ? person.yearOfDeath : (new Date()).getFullYear()) - person.yearOfBirth;
+    return oldestPersonAge > newPersonAge ? oldest : person;
+  }, people[0]);
 };
 
 // Do not edit below this line
